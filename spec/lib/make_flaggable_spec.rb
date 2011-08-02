e40c25ab8efbbcd4b633eca837645181ff68a796
Jaime Iniesta <jaimeiniesta@gmail.com>
2011-08-02 14:18:29 +0200

    Specs style cleanup

--- spec/lib/make_flaggable_spec.rb
+++ spec/lib/make_flaggable_spec.rb
@@ -2,8 +2,8 @@ require File.expand_path('../../spec_helper', __FILE__)
 
 describe "Make Flaggable" do
   before(:each) do
-    @flaggable = FlaggableModel.create(:name => "Flaggable 1")
-    @flagger = FlaggerModel.create(:name => "Flagger 1")
+    @flaggable  = FlaggableModel.create(:name => "Flaggable 1")
+    @flagger    = FlaggerModel.create(:name => "Flagger 1")
   end
 
   it "should create a flaggable instance" do
@@ -21,7 +21,7 @@ describe "Make Flaggable" do
     @flagger.flag!(@flaggable, :inappropriate)
     @flagger.flaggings.reload.length.should == 1
   end
-  
+
   describe "flagger" do
     describe "flag" do
       it "should create a flagging" do
@@ -29,7 +29,7 @@ describe "Make Flaggable" do
         @flagger.flag!(@flaggable, :inappropriate)
         @flaggable.flaggings.reload.length.should == 1
       end
-      
+
       it "should not allow duplicate flaggings with the same flag and flaggable and throw exception" do
         @flaggable.flaggings.length.should == 0
         @flagger.flag!(@flaggable, :inappropriate)
@@ -37,14 +37,14 @@ describe "Make Flaggable" do
           @flagger.flag!(@flaggable, :inappropriate)
         }.should raise_error(MakeFlaggable::Exceptions::AlreadyFlaggedError)
       end
-      
+
       it "should not allow duplicate flaggings with the same flag and flaggable and return false" do
         @flaggable.flaggings.length.should == 0
         @flagger.flag!(@flaggable, :inappropriate)
         @flagger.flag(@flaggable, :inappropriate).should == nil
       end
     end
-  
+
     describe "unflag" do
       it "should unflag a flagging" do
         @flagger.flag!(@flaggable, :inappropriate)
@@ -52,7 +52,7 @@ describe "Make Flaggable" do
         @flagger.unflag!(@flaggable, :inappropriate).should == true
         @flagger.flaggings.reload.length.should == 0
       end
-      
+
       it "should unflag individual flaggings based on the flag" do
         @flagger.flag!(@flaggable, :inappropriate)
         @flagger.flag!(@flaggable, :favorite)
@@ -60,25 +60,25 @@ describe "Make Flaggable" do
         @flagger.unflag!(@flaggable, :inappropriate).should == true
         @flagger.flaggings.reload.length.should == 1
       end
-      
+
       it "normal method should return true when successfully unflagged" do
         @flagger.flag(@flaggable, :favorite)
         @flagger.unflag(@flaggable, :favorite).should == true
       end
-      
+
       it "should raise error if flagger not flagged the flaggable with bang method" do
         lambda { @flagger.unflag!(@flaggable, :favorite) }.should raise_error(MakeFlaggable::Exceptions::NotFlaggedError)
       end
-      
+
       it "should not raise error if flagger not flagged the flaggable with normal method" do
         lambda {
           @flagger.unflag(@flaggable, :favorite).should == false
         }.should_not raise_error(MakeFlaggable::Exceptions::NotFlaggedError)
       end
     end
-      
+
     describe "flagged?" do
-      it "should check if flagger is flagged the flaggable" do
+      it "should check if flagger has flagged the flaggable" do
         @flagger.flagged?(@flaggable, :favorite).should == false
         @flagger.flag!(@flaggable, :favorite)
         @flagger.flagged?(@flaggable, :favorite).should == true
@@ -87,14 +87,14 @@ describe "Make Flaggable" do
       end
     end
   end
-  
+
   describe "flaggable" do
     it "should have flaggings" do
       @flaggable.flaggings.length.should == 0
       @flagger.flag!(@flaggable, :favorite)
       @flaggable.flaggings.reload.length.should == 1
     end
-  
+
     it "should check if flaggable is flagged" do
       @flaggable.flagged?.should == false
       @flagger.flag!(@flaggable, :favorite)
